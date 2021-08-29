1d511bf6c36c1be66214def4178c705788224719
vitorsamora <vitor.samora@gmail.com>
2021-08-29 15:05:41 -0300

    Add starter code (generated by Spring Initializr)

---
+++ src/test/java/br/com/elo7/exploringmars/ExploringMarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elo7.exploringmars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploringMarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
